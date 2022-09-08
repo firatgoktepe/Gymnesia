f487419c1413a919a252e0712773a1284f49f190
DeveloperFG <firatgoktepe1986@gmail.com>
2022-09-08 12:48:52 +0300

    feat: gif and rep endpoints added

--- db.js
+++ db.js
@@ -4,36 +4,44 @@ export const workouts = [
         "imageLink": require('./assets/images/workouts/bench_press.jpeg'),
         "title": "Bench Press",
         "id": 1,
-        "description": "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
+        "description": "The bench press, or chest press, is a weight training exercise in which the trainee presses a weight upwards while lying on a weight training bench. Although the bench press is a full-body exercise, the muscles primarily used are the pectoralis major, the anterior deltoids, and the triceps, among other stabilizing muscles. A barbell is generally used to hold the weight, but a pair of dumbbells can also be used. The barbell bench press is one of three lifts in the sport of powerlifting alongside the deadlift and squat, and is the only lift in the sport of Paralympic powerlifting. It is also used extensively in weight training, bodybuilding, and other types of training to develop the chest muscles. Bench press strength is important in combat sports as it tightly correlates to punching power. Bench press can also help contact athletes increase their performance because it can increase effective mass and functional hypertrophy of the upper body.",
         "duration": 24,
-        "task": 34
+        "task": 34,
+        "gif": {uri: 'http://d205bpvrqc9yn1.cloudfront.net/1254.gif'},
+        "rep": 3
       },
       {
         "calories": 1000,
         "imageLink": require('./assets/images/workouts/pushup.jpeg'),
         "title": "Pushup",
         "id": 2,
-        "description": "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.", 
+        "description": "The push-up (sometimes called a press-up in British English) is a common calisthenics exercise beginning from the prone position. By raising and lowering the body using the arms, push-ups exercise the pectoral muscles, triceps, and anterior deltoids, with ancillary benefits to the rest of the deltoids, serratus anterior, coracobrachialis and the midsection as a whole. Push-ups are a basic exercise used in civilian athletic training or physical education and commonly in military physical training. They are also a common form of punishment used in the military, school sport, and some martial arts disciplines.", 
         "duration": 30,
-        "task": 24
+        "task": 24,
+        "gif": {uri: 'http://d205bpvrqc9yn1.cloudfront.net/1311.gif'},
+        "rep": 3
       },
       {
         "calories": 1300,
         "imageLink": require('./assets/images/workouts/resistance_band.jpeg'),
         "title": "Resistance",
         "id": 3,
-        "description": 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.',
+        "description": 'A resistance band is an elastic band used for strength training. They are also commonly used in physical therapy, specifically by convalescents of muscular injuries, including cardiac rehab patients to allow slow rebuilding of strength. Originating in the early 20th century, the bands were originally made from surgical tubing and the exercises conducted for muscle rehabilitation, and resistance band training is now used widely as part of general fitness and strength training. Their flexibility in use and light weight are a significant advantage for many users.',
         "duration": 45,
-        "task": 10
+        "task": 10,
+        "gif": {uri: 'http://d205bpvrqc9yn1.cloudfront.net/3236.gif'},
+        "rep": 4
       },
       {
         "calories": 1500,
         "imageLink": require('./assets/images/workouts/full_body.jpeg'),
         "title": "Full Body",
         "id": 4,
-        "description": 'The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.',
+        "description": 'A full body workout is just what it sounds like: a workout that aims to hit all the major muscle groups in one single session. Popular programs would include exercises for back, legs, chest, shoulders, arms and core. There are many reasons some trainees like to utilise a full body workout, whether from time to time or as a standard go-to in their workout routine. “When performing full-body workouts, you don’t want to waste time doing isolation exercises,” says personal trainer Matt Hampton. “You want to get the biggest, hardest exercises done for the biggest muscle stimulation.”',
         "duration": 35,
-        "task": 20
+        "task": 20,
+        "gif": {uri: 'http://d205bpvrqc9yn1.cloudfront.net/3220.gif'},
+        "rep": 3
       },
     ]
   
