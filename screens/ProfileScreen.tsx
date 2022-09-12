64c9832087a0a76ae05195e0ccbf70a5f8eaf83a
DeveloperFG <firatgoktepe1986@gmail.com>
2022-09-12 18:45:22 +0300

    profile screen modified

--- screens/ProfileScreen.tsx
+++ screens/ProfileScreen.tsx
@@ -1,31 +1,57 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Pressable, Image } from "react-native";
+import { loggingOut } from "../API/firebaseMethods";
+import { Text, View } from "../components/Themed";
+import { getAuth } from "firebase/auth";
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
 
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
+const ProfileScreen: React.FC<any> = ({ navigation }) => {
+  const auth = getAuth();
+  const user = auth.currentUser;
+  if (user !== null) {
+    const email = user.email;
 
-export default function ProfileScreen() {
+    const uid = user.uid;
+  }
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Profile</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/ProfileScreen.tsx" />
+      <Image
+        style={styles.profileLogo}
+        source={require("../assets/images/profile.jpeg")}
+      />
+      <Text style={[styles.title, { color: "#25AB75", fontStyle: "italic" }]}>
+        {user && user.email}
+      </Text>
+      <Pressable onPress={() => loggingOut()}>
+        <Text style={[styles.title, { color: "#F95045", fontSize: 25 }]}>
+          Logout
+        </Text>
+      </Pressable>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
+    marginBottom: 20,
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+  profileLogo: {
+    width: 250,
+    height: 250,
+    borderRadius: 150,
+    marginRight: 10,
+    marginTop: 20,
+    marginBottom: 20,
   },
 });
+
+export default ProfileScreen;
