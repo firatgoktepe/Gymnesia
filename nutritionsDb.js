68bbaf2778ef9f078c6e5781900bb4c16568ba78
DeveloperFG <firatgoktepe1986@gmail.com>
2022-09-15 17:33:40 +0300

    feat: dummy nutrition data created

---
+++ nutritionsDb.js
@@ -0,0 +1,83 @@
+export const nutritions = [
+    {
+        "id": 1,
+        "title": "Bacon King Burger",
+        "nutrition": {
+            "nutrients": [
+                {
+                    "name": "Fat",
+                    "amount": 69,
+                    "unit": "g",
+                    "percentOfDailyNeeds": 30
+                },
+                {
+                    "name": "Protein",
+                    "amount": 57,
+                    "unit": "g",
+                    "percentOfDailyNeeds": 35
+                },
+                {
+                    "name": "Calories",
+                    "amount": 1040,
+                    "unit": "cal",
+                    "percentOfDailyNeeds": 50
+                },
+                {
+                    "name": "Carbohydrates",
+                    "amount": 48,
+                    "unit": "g",
+                    "percentOfDailyNeeds": 35
+                }
+            ],
+        },
+        "categories": "Dinner",
+        "imageType": "png",
+        "servings": {
+            "number": 1,
+            "size": 2,
+            "unit": "oz"
+        },
+    },
+
+    {
+        "id": 2,
+        "title": "Wheat Flour Durum",
+        "nutrition": {
+            "nutrients": [
+                {
+                    "name": "Fat",
+                    "amount": 39,
+                    "unit": "g",
+                    "percentOfDailyNeeds": 30
+                },
+                {
+                    "name": "Protein",
+                    "amount": 47,
+                    "unit": "g",
+                    "percentOfDailyNeeds": 35
+                },
+                {
+                    "name": "Calories",
+                    "amount": 1040,
+                    "unit": "cal",
+                    "percentOfDailyNeeds": 50
+                },
+                {
+                    "name": "Carbohydrates",
+                    "amount": 60,
+                    "unit": "g",
+                    "percentOfDailyNeeds": 35
+                }
+            ],
+        },
+        "categories": "Dinner",
+        "imageType": "png",
+        "servings": {
+            "number": 1,
+            "size": 2,
+            "unit": "oz"
+        },
+    },
+
+
+]
