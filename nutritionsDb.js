6dff8593810234260364f038760b6a45a82aa619
DeveloperFG <firatgoktepe1986@gmail.com>
2022-09-16 10:58:22 +0300

    feat: nutritions db modified

--- nutritionsDb.js
+++ nutritionsDb.js
@@ -31,7 +31,7 @@ export const nutritions = [
             ],
         },
         "categories": "Dinner",
-        "imageType": "png",
+        "imageLink": "https://images.unsplash.com/photo-1568901346375-23c9450c58cd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=699&q=80",
         "servings": {
             "number": 1,
             "size": 2,
@@ -70,13 +70,99 @@ export const nutritions = [
                 }
             ],
         },
-        "categories": "Dinner",
-        "imageType": "png",
+        "categories": "Lunch",
+        "imageLink": "https://images.unsplash.com/photo-1600626333540-9d9ca017046c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=937&q=80",
         "servings": {
             "number": 1,
             "size": 2,
             "unit": "oz"
         },
+
+        
+    },
+
+    {
+        "id": 3,
+        "title": "Kebab Durum",
+        "nutrition": {
+            "nutrients": [
+                {
+                    "name": "Fat",
+                    "amount": 69,
+                    "unit": "g",
+                    "percentOfDailyNeeds": 40
+                },
+                {
+                    "name": "Protein",
+                    "amount": 57,
+                    "unit": "g",
+                    "percentOfDailyNeeds": 35
+                },
+                {
+                    "name": "Calories",
+                    "amount": 1090,
+                    "unit": "cal",
+                    "percentOfDailyNeeds": 55
+                },
+                {
+                    "name": "Carbohydrates",
+                    "amount": 70,
+                    "unit": "g",
+                    "percentOfDailyNeeds": 25
+                }
+            ],
+        },
+        "categories": "Dinner",
+        "imageLink": "https://images.unsplash.com/photo-1633321702518-7feccafb94d5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+        "servings": {
+            "number": 1,
+            "size": 2,
+            "unit": "oz"
+        },
+
+        
+    },
+
+    {
+        "id": 4,
+        "title": "Omelet",
+        "nutrition": {
+            "nutrients": [
+                {
+                    "name": "Fat",
+                    "amount": 29,
+                    "unit": "g",
+                    "percentOfDailyNeeds": 40
+                },
+                {
+                    "name": "Protein",
+                    "amount": 47,
+                    "unit": "g",
+                    "percentOfDailyNeeds": 35
+                },
+                {
+                    "name": "Calories",
+                    "amount": 1090,
+                    "unit": "cal",
+                    "percentOfDailyNeeds": 55
+                },
+                {
+                    "name": "Carbohydrates",
+                    "amount": 70,
+                    "unit": "g",
+                    "percentOfDailyNeeds": 25
+                }
+            ],
+        },
+        "categories": "Breakfast",
+        "imageLink": "https://images.unsplash.com/photo-1525351484163-7529414344d8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80",
+        "servings": {
+            "number": 1,
+            "size": 2,
+            "unit": "oz"
+        },
+
+        
     },
 
 
