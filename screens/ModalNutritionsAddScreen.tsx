86ac9b9efa7695537d703b804ba10ea0760146ce
DeveloperFG <firatgoktepe1986@gmail.com>
2022-09-19 12:35:19 +0300

    feat: nutritions add screen added

---
+++ screens/ModalNutritionsAddScreen.tsx
@@ -0,0 +1,151 @@
+import React from "react";
+import * as WebBrowser from "expo-web-browser";
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  ScrollView,
+  Image,
+  TouchableOpacity,
+  Pressable,
+  TextInput,
+} from "react-native";
+import Colors from "../constants/Colors";
+import { MonoText } from "../components/StyledText";
+import { View, Text } from "../components/Themed";
+import { EvilIcons, Ionicons } from "@expo/vector-icons";
+import Search from "../components/Search";
+import { workouts } from "../db";
+import { HeadingText } from "../components/Headings";
+import { WorkoutsDb } from "../components/SingleCards";
+import { useRoute } from "@react-navigation/native";
+import { nutritions } from "../nutritionsDb";
+
+export default function ModalNutritionsAddScreen() {
+  const route = useRoute();
+  const {
+    id,
+    title,
+    image,
+    nutrientsFat,
+    nutrientsFatAmount,
+    nutrientsFatUnit,
+    nutrientsProtein,
+    nutrientsProteinAmount,
+    nutrientsProteinUnit,
+    nutrientsCalories,
+    nutrientsCaloriesAmount,
+    nutrientsCaloriesUnit,
+    nutrientsCarbonhydrates,
+    nutrientsCarbonhydratesAmount,
+    nutrientsCarbonhydratesUnit,
+  }: any = route.params;
+  const [number, onChangeNumber] = React.useState("");
+
+  function handleHelpPress() {
+    WebBrowser.openBrowserAsync(
+      "https://www.momsteam.com/nutrition/sports-nutrition-basics/nutritional-needs-guidelines/carbohydrate-and-calorie-content-of-foods"
+    );
+  }
+
+  return (
+    <View style={styles.cardContainer}>
+      <View key={id} style={styles.container}>
+        <Image
+          style={{ width: 100, height: 100, borderRadius: 150 }}
+          source={{
+            uri: image,
+          }}
+        />
+        <View style={styles.card}>
+          <Text style={styles.title}>{title}</Text>
+          <View style={styles.values}>
+            <Text>
+              {nutrientsFat + ":"} {nutrientsFatAmount} {nutrientsFatUnit + " "}
+            </Text>
+            <Text>
+              {nutrientsProtein + ":"} {nutrientsProteinAmount}{" "}
+              {nutrientsProteinUnit}
+            </Text>
+            <Text>
+              {nutrientsCarbonhydrates + ":"} {nutrientsCarbonhydratesAmount}{" "}
+              {nutrientsCarbonhydratesUnit}
+            </Text>
+            <Text style={{ fontWeight: "bold" }}>
+              {nutrientsCalories + ":"} {nutrientsCaloriesAmount}{" "}
+              {nutrientsCaloriesUnit}
+            </Text>
+          </View>
+        </View>
+      </View>
+      <Text style={[styles.title, { marginLeft: 10, marginTop: 10 }]}>
+        Enter the amount you ate ( grams )
+      </Text>
+      <TextInput
+        value={number}
+        onChangeText={onChangeNumber}
+        style={styles.input}
+        placeholder="100"
+        keyboardType="numeric"
+      />
+      <View>
+        <TouchableOpacity style={styles.button} onPress={handleHelpPress}>
+          <MonoText style={styles.buttonText} lightColor={Colors.light.tint}>
+            More Details
+          </MonoText>
+        </TouchableOpacity>
+      </View>
+      <Pressable style={styles.button}>
+        <Text style={styles.buttonText}>Done</Text>
+      </Pressable>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  cardContainer: { flex: 1 },
+  container: {
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    marginBottom: 5,
+    marginTop: 10,
+    padding: 10,
+  },
+  card: { justifyContent: "center" },
+  values: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    width: 200,
+    justifyContent: "flex-start",
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 15,
+    lineHeight: 24,
+    fontWeight: "bold",
+  },
+  button: {
+    width: 350,
+    padding: 5,
+    backgroundColor: "#25AB75",
+    borderWidth: 2,
+    borderColor: "white",
+    borderRadius: 15,
+    alignSelf: "center",
+    margin: "5%",
+    flexDirection: "row",
+    justifyContent: "center",
+  },
+  buttonText: {
+    fontSize: 20,
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center",
+    padding: 10,
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+});
