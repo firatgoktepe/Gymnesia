051b6f25c174e08325008553c7a80fcc8962f681
DeveloperFG <firatgoktepe1986@gmail.com>
2022-10-12 16:01:35 +0300

    feat: unit tests added for Search component

---
+++ components/__tests__/Search-test.js
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import renderer from 'react-test-renderer';
+import { MonoText } from '../StyledText';
+import Search  from '../Header';
+import {render, cleanup} from '@testing-library/react-native';
+
+afterEach(cleanup);
+
+  describe('<Search />', () => {
+    it('has 2 children', () => {
+      const tree = renderer.create(<Search />).toJSON();
+      expect(tree.children.length).toBe(2);
+    });
+
+    it('renders correctly', () => {
+        const tree = renderer.create(<Search />).toJSON();
+        expect(tree).toMatchSnapshot();
+      });
+
+  });
