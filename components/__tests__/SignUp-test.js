a18eaff96e2d4f49dae6cf93e4f4bc786955845a
DeveloperFG <firatgoktepe1986@gmail.com>
2022-10-13 15:42:45 +0300

    feat: added tests for SignUp component

---
+++ components/__tests__/SignUp-test.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import SignUp from '../../screens/SignUp';
+import {render, cleanup, fireEvent} from '@testing-library/react-native';
+
+afterEach(cleanup);
+
+describe('<SignUp />', () => {
+  it('should match the snapshot', () => {
+    const rendered = render(<SignUp />).toJSON();
+    console.log("Sign in rendered", rendered.children[1]);
+    expect(rendered).toMatchSnapshot();
+  });
+  
+  it('should fire onChange events', () => {
+    const {getByTestId} = render(<SignUp />);
+    const nameInput = getByTestId('inputName');
+    const surnameInput = getByTestId('inputSurName');
+    const emailInput = getByTestId('inputEmail');
+    const passwordInput = getByTestId('inputPassword');
+    const passwordConfirmInput = getByTestId('inputConfirmPassword');
+    fireEvent.changeText(emailInput, 'test');
+    fireEvent.changeText(passwordInput, 'test');
+    fireEvent.changeText(passwordConfirmInput, 'test');
+    fireEvent.changeText(nameInput, 'test');
+    fireEvent.changeText(surnameInput, 'test');
+    expect(emailInput.props.value).toEqual('test');
+    expect(passwordInput.props.value).toEqual('test');
+    expect(passwordConfirmInput.props.value).toEqual('test');
+    expect(nameInput.props.value).toEqual('test');
+    expect(surnameInput.props.value).toEqual('test');
+  });
+
+  it('should have true placeholder names', () => {
+    const {getByTestId} = render(<SignUp />);
+    const nameInput = getByTestId('inputName');
+    const surnameInput = getByTestId('inputSurName');
+    const emailInput = getByTestId('inputEmail');
+    const passwordInput = getByTestId('inputPassword');
+    const passwordConfirmInput = getByTestId('inputConfirmPassword');
+    expect(emailInput.props.placeholder).toEqual('Enter your email*');
+    expect(passwordInput.props.placeholder).toEqual('Enter your password*');
+    expect(passwordConfirmInput.props.placeholder).toEqual('Retype your password to confirm*');
+    expect(nameInput.props.placeholder).toEqual('First name*');
+    expect(surnameInput.props.placeholder).toEqual('Last name*');
+  })
+  
+});
